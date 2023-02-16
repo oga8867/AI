92129a83b1f89ac2f720d3a8ac28171f5205c5fe
oga8867 <30932394+oga8867@users.noreply.github.com>
2023-02-16 10:55:38 +0900

    Update main.py

--- test_webcam/webcam_streamlit/main.py
+++ test_webcam/webcam_streamlit/main.py
@@ -1,16 +1,22 @@
-import streamlit as st
 # import numpy as np
 # import pandas as pd
 # import io
 # import os
 # from  PIL import Image
 # import cv2
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+# from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-def app():
-    st.title("Webcam Test")
-    webrtc_ctx = webrtc_streamer(key="example")#, video_transformer_factory=VideoTransformer)
+# def app():
+#     st.title("Webcam Test")
+#     webrtc_ctx = webrtc_streamer(key="example")#, video_transformer_factory=VideoTransformer)
 
-app()
+# app()
+
+import streamlit as st
+
+picture = st.camera_input("Take a picture")
+
+if picture:
+    st.image(picture)
 
 #실행   streamlit run main.py
