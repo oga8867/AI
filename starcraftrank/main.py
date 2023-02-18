8a36e07cbd07a2afb616cc5e3e425549e2831ed3
oga8867 <30932394+oga8867@users.noreply.github.com>
2023-02-18 23:14:56 +0900

    Add files via upload

--- starcraftrank/main.py
+++ starcraftrank/main.py
@@ -62,12 +62,55 @@ def starcraft_model():
     # clf.score(x_test, y_test) # 모형 성능 평가
 
     joblib.dump(LR, './starcraft.pkl')
-#starcraft_model()  #위에 모델 실행
-model_from_joblib = joblib.load('./starcraft.pkl')
+#starcraft_model()  #위에 리니어모델 실행
+model_from_joblib = joblib.load('./starcraft.pkl')  #리니어모델 불러오기
 #print(model_from_joblib.predict(x_test)) 모델 불러와서 예측
 
-st.write('Most objects') # df, err, func, keras!
+def logistic_model():
+    cause = pd.DataFrame(a.iloc[:,[2,3,4,5,6,13]]) #-1이 스킬사용인데, 데이터가 적어서 인지 정확하지 않아서 제외하기로 함
+    consequence = pd.DataFrame(a.LeagueIndex)
+    # print(cause)
+    # print(consequence)
 
+    df = pd.concat([cause, consequence], axis=1)
+
+    # df['Age'] = df['Age'].replace('?', df['Age'].mean())
+    # df['HoursPerWeek'] = df['HoursPerWeek'].replace('?', df['HoursPerWeek'].mean())
+    # df['HTotalHours'] = df['TotalHours'].replace('?', df['TotalHours'].mean())
+
+    df = df.replace('?', np.nan)
+    df = df.dropna()
+
+
+    # 시각화
+    # sns.pairplot(df[["Age", "HoursPerWeek", "TotalHours", "APM","SelectByHotkeys","ActionLatency","LeagueIndex"]])
+    #plt.show()
+
+
+    # scaler = StandardScaler()
+    # df = scaler.fit_transform(df)
+
+    x = df[["Age", "HoursPerWeek", "TotalHours", "APM","SelectByHotkeys","ActionLatency"]]
+    y = df[["LeagueIndex"]]
+    #x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.25, random_state=0) #스플릿해야하면 이걸 넣어야함 그리고 나서 핏에 트레인들을 넣어주고 예측에 x테스트를 넣어줘야함
+
+    # print(x_train.describe())
+    # print(y_train.describe())
+    # exit()
+
+
+
+    LR = LogisticRegression()
+    LR.fit(x,y)  # 모수 추정
+    # clf.coef_                 # 추정 된 모수 확인(상수항 제외)
+    # clf.intercept_            # 추정 된 상수항 확인
+    #LR.predict(x_test)        # 예측
+    # clf.score(x_test, y_test) # 모형 성능 평가
+
+    joblib.dump(LR, './starcraft_logistic.pkl')
+
+#logistic_model()  #위에 로지스틱모델 실행
+Logimodel_from_joblib = joblib.load('./starcraft_logistic.pkl')  #로지스틱모델 불러오기
 
 def linear_streamlit():
     age = float(st.number_input('pick your age', 0, 99))
@@ -85,35 +128,89 @@ def linear_streamlit():
         predict = int(round(float(anw[[0],[0]]),0))
         st.write('당신의 예측결과 : ',rankshoots(predict))
         if rankshoots(predict):
-            if predict == 1:
+            if predict <= 0:
+                images = ''
+            elif predict == 1:
                 images = './images/bronze.png'
+                st.image(f'{images}')
             elif predict == 2:
                 images = './images/silver.png'
+                st.image(f'{images}')
             elif predict == 3:
                 images = './images/gold.png'
+                st.image(f'{images}')
             elif predict == 4:
                 images = './images/platinum.png'
+                st.image(f'{images}')
             elif predict == 5:
                 images = './images/diamond.png'
+                st.image(f'{images}')
             elif predict == 6:
                 images = './images/master.png'
+                st.image(f'{images}')
             elif predict == 7:
                 images = './images/GM.png'
-            elif predict == 8:
+                st.image(f'{images}')
+            elif predict >= 8:
                 images = './images/progamer.png'
-        st.image(f'{images}')
+                st.image(f'{images}')
+            else:
+                st.write('무언가 오류가있습니다.')
+
         #round(model_from_joblib.predict(x),1)
-linear_streamlit()
+def logistic_streamlit():
+    age = float(st.number_input('pick your age', 0, 99))
+    apm = float(st.number_input('apm of this game', 0, 1000))
+    wholePT = float(st.number_input('playtime of all your life', 0, 100000))
+    weekPT = float(st.number_input('playtime of this week', 0, 168))
+    selectkey = float(st.number_input('how many use selectkey(단축키) of this game?', 0, 10000) / 88.5)
+    AL = float(st.number_input('your actionlatency(ms)', 0, 1000000))
+    enterBT = st.button('Click me')
+    if enterBT:
+        x = pd.DataFrame([["Age", "HoursPerWeek", "TotalHours", "APM", "SelectByHotkeys", "ActionLatency"]])
+        x.loc[0] = [age, apm, wholePT, weekPT, selectkey, AL]
+        anw = Logimodel_from_joblib.predict(x)
+        # st.write(model_from_joblib.predict(x))  # see *
+        predict = int(round(float(anw[[0], [0]]), 0))
+        st.write('당신의 예측결과 : ', rankshoots(predict))
+        if rankshoots(predict):
+            if predict <= 0:
+                images = ''
+            elif predict == 1:
+                images = './images/bronze.png'
+                st.image(f'{images}')
+            elif predict == 2:
+                images = './images/silver.png'
+                st.image(f'{images}')
+            elif predict == 3:
+                images = './images/gold.png'
+                st.image(f'{images}')
+            elif predict == 4:
+                images = './images/platinum.png'
+                st.image(f'{images}')
+            elif predict == 5:
+                images = './images/diamond.png'
+                st.image(f'{images}')
+            elif predict == 6:
+                images = './images/master.png'
+                st.image(f'{images}')
+            elif predict == 7:
+                images = './images/GM.png'
+                st.image(f'{images}')
+            elif predict >= 8:
+                images = './images/progamer.png'
+                st.image(f'{images}')
+            else:
+                st.write('무언가 오류가있습니다.')
 
-# def run_home() :
-#     st.subheader("predict your stacraft RANK!")
-#     tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-#     tab1.write("Linear Regression")
-#     if tab1:
-#
-#     tab2.write("this is tab 2")
-#
-#     number_of_news = st.slider('number of news to you want know', min_value=1, max_value=10)
-#     name = st.text_input('Search your stock!')
-
+def run_home() :
+    st.subheader("predict your stacraft RANK!")
+    tab1, tab2 = st.tabs(["Linear", "Logistic"])
+    tab1.write("Linear Regression")
+    tab2.write("Logistic")
+    if tab1:
+        linear_streamlit()
+    elif tab2:
+        logistic_streamlit()
+run_home()
 
